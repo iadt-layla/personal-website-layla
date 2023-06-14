614fb427ba6d774f40bad201d8d7c958b818804c
iadt-layla <136593760+iadt-layla@users.noreply.github.com>
2023-06-14 15:39:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-let pic = document.getElementById("profile-picture");
+var img = document.getElementById("gallery-img");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
 
-document.addEventListener("scroll", function () {
-  if (window.scrollY > 5) {
-    pic.style.borderRadius = "0%";
-  } else {
-    pic.style.borderRadius = "100%";
+// if you have more images than me, type their names in here...
+var images = ["hedgehog.jpg", "man.jpg"];
+
+var currentimage = 0;
+
+function changeimage(e) {
+  if (e.target.id == "next" && currentimage !== images.length - 1) {
+    // increment the images (in other words, go to the next one)
+    currentimage = currentimage + 1;
   }
-});
+
+  if (e.target.id == "prev" && currentimage !== 0) {
+    // logic goes in here
+    currentimage = currentimage - 1;
+  }
+
+  img.setAttribute("src", "images/" + images[currentimage]);
+}
+
+prev.addEventListener("click", changeimage);
+next.addEventListener("click", changeimage);
